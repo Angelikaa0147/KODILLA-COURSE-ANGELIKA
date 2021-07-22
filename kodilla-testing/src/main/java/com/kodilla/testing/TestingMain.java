a743f88b29cb0b41f48942370047a557e9e3c761
Angelikaa0147 <angelika.solak@wp.pl>
2021-07-22 16:35:49 +0200

    JUNIT testy - problem z ASSERCJAMI

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,4 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theUser");
+
+        String result = simpleUser.getUserName();
+        if(result.equals("theUser")){
+            System.out.println("OK");
+        }else System.out.println("ERROR");
+
+        //test for caltulator
+
+        Calculator calculator = new Calculator();
+        int resultAdd = calculator.add(2,5);
+        if(resultAdd==7){
+            System.out.println("OK");
+        }else System.out.println("ERROR");
+
+    }
 }
